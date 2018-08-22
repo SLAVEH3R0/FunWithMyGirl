77f04805f08837b79ebcd6e019a7fd296038a0fa
SLAVEH3R0 <francislavallee1996@hotmail.com>
2018-08-21 21:33:47 -0400

    Début de l'affichage de la page horaire complet

--- MonHoraire.py
+++ MonHoraire.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 import MonHoraireInterface
 
-MonHoraireInterface.AffichageInterfaceAcceuilHoraire()
+MonHoraireInterface.affichageInterfaceAcceuilHoraire()
--- MonHoraireInterface.py
+++ MonHoraireInterface.py
@@ -1,14 +1,25 @@
-from tkinter import *
+import tkinter as tk
 
-AffichagePrincipalHoraire = Tk()
-AffichagePrincipalHoraire.geometry('500x500')
+AffichagePrincipalHoraire = tk.Tk()
+AffichagePrincipalHoraire.geometry("1600x800+0+0")
 AffichagePrincipalHoraire.title('Planification de l\'horaire de XXX')
 
-def AffichageInterfaceAcceuilHoraire():
+def affichageInterfaceAcceuilHoraire():
     horaireComplet()
     AffichagePrincipalHoraire.mainloop()
 
 def horaireComplet():
-    labelHoraireComplet = Label(AffichagePrincipalHoraire, text='Horaire complet',font=("Helvetica", 16))
+    #Définition de la section du haut
+    frame_Tops = tk.Frame(AffichagePrincipalHoraire,width = 160,bg="powder blue")
+    frame_Tops.pack(side=tk.TOP)
+    #définition de la section de d'affichage de l'horaire de gauche
+    frame_HoraireCompletLeft = tk.Frame(AffichagePrincipalHoraire,width = 130, height= 70,bg="powder blue")
+    frame_HoraireCompletLeft.pack(side=tk.LEFT)
+    #définition de la section de d'affichage de l'horaire de droit
+    frame_HoraireCompletRight = tk.Frame(AffichagePrincipalHoraire,width = 130, height= 70,bg="powder blue")
+    frame_HoraireCompletRight.pack(side=tk.RIGHT)
+
+    labelHoraireComplet = tk.Label(frame_Tops, text='Horaire complet',font=("Times", 50, 'bold'), fg="Steel Blue")
+    labelHoraireComplet.grid(row=0,column=0)
     labelHoraireComplet.pack()
     #Instentiation du tableau
